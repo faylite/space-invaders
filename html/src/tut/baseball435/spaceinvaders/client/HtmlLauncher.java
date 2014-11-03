2a5acc8e61959e9e2734420bc89810bd1f846ccd
Benjamin Vevang <benvev95@gmail.com>
2014-11-03 19:37:26 +0100

    Initial Commit

---
+++ html/src/tut/baseball435/spaceinvaders/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package tut.baseball435.spaceinvaders.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import tut.baseball435.spaceinvaders.MainGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MainGame();
+        }
+}
